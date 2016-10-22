1f5f7c3c3c3cc72f21feed9641cc452a7c1cfa5f
Yu Zhu <yz6@ualberta.ca>
2016-10-22 14:23:35 -0600

    update

---
+++ app/src/main/java/ca/ualberta/cs/unter/DriverBrowseRequestActivity.java
@@ -0,0 +1,13 @@
+package ca.ualberta.cs.unter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DriverBrowseRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_browse_request);
+    }
+}
