1b0a4fdb09dc8ae36a3476dea0fa7559355696e3
Michael Lin <linzichunzf@hotmail.com>
2016-10-27 14:02:11 -0600

    add global constant

---
+++ app/src/main/java/ca/ualberta/cs/unter/UnterConstant.java
@@ -0,0 +1,7 @@
+
+package ca.ualberta.cs.unter;
+
+public class UnterConstant {
+    public static final String ELASTIC_SEARCH_URL = "http://cmput301.softwareprocess.es:8080";
+    public static final String LOCAL_STORAGE_FILENAME = "xx.json";
+}
