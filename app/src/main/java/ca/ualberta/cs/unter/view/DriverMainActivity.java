ff29c78dd8e6c62b838d1ca5627bfcbba5117da6
Yu Zhu <yz6@ualberta.ca>
2016-11-06 14:42:25 -0700

    request adapter for list view in DriverMainActivity

--- app/src/main/java/ca/ualberta/cs/unter/view/DriverMainActivity.java
+++ app/src/main/java/ca/ualberta/cs/unter/view/DriverMainActivity.java
@@ -14,16 +14,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
+
 import ca.ualberta.cs.unter.R;
+import ca.ualberta.cs.unter.model.request.Request;
 
 public class DriverMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private Spinner searchOptionSpinner;
     private ArrayAdapter<CharSequence> searchOptionAdapter;
+
+    private ListView searchRequestListView;
+    private ArrayAdapter<Request> searchRequestAdapter;
+    private ArrayList<Request> searchRequestList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +48,18 @@ public class DriverMainActivity extends AppCompatActivity
         searchOptionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         searchOptionSpinner.setAdapter(searchOptionAdapter);
 
+        searchRequestListView = (ListView) findViewById(R.id.listView_searchList_DriverMainActivity);
+        searchRequestListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // TODO intent to DriverAcceptRequestActivity
+//                Intent intentDriverAcceptRequest = new Intent(this, DriverAcceptRequestActivity.class);
+//                startActivity(intentDriverAcceptRequest);
+            }
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -52,6 +71,13 @@ public class DriverMainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         assert navigationView != null;
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        searchRequestAdapter = new ArrayAdapter<>(this, R.layout.driver_accepted_list_item, searchRequestList);
+        searchRequestListView.setAdapter(searchRequestAdapter);
     }
 
     @Override
