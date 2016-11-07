02205befa2b45f3b4c7198e8cb80381f640adba5
Michael Lin <linzichunzf@hotmail.com>
2016-11-06 21:21:18 -0700

    driver can search request by keyword driver can accept request

--- app/src/main/java/ca/ualberta/cs/unter/view/DriverSearchRequestActivity.java
+++ app/src/main/java/ca/ualberta/cs/unter/view/DriverSearchRequestActivity.java
@@ -1,9 +1,10 @@
 package ca.ualberta.cs.unter.view;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,8 +18,15 @@ import java.util.ArrayList;
 import ca.ualberta.cs.unter.R;
 import ca.ualberta.cs.unter.controller.RequestController;
 import ca.ualberta.cs.unter.model.OnAsyncTaskCompleted;
+import ca.ualberta.cs.unter.model.User;
 import ca.ualberta.cs.unter.model.request.Request;
+import ca.ualberta.cs.unter.util.FileIOUtil;
+import ca.ualberta.cs.unter.util.RequestIntentUtil;
 
+/**
+ * Activity that driver can search for request
+ * and browse the map and accept it
+ */
 public class DriverSearchRequestActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Spinner searchOptionSpinner;
@@ -31,13 +39,22 @@ public class DriverSearchRequestActivity extends AppCompatActivity implements Vi
     private ArrayAdapter<Request> searchRequestAdapter;
     private ArrayList<Request> searchRequestList = new ArrayList<>();
 
+    private int searchOption;
+    private User driver;
+
+    // Requestcontroller for searching result
     private RequestController requestController = new RequestController(new OnAsyncTaskCompleted() {
         @Override
         public void onTaskCompleted(Object o) {
             // Cast
-            searchRequestList = (ArrayList<Request>) o;
-            for (Request r : searchRequestList) {
-                Log.i("Debug", r.toString());
+            ArrayList<Request> getRequest = (ArrayList<Request>) o;
+            searchRequestList.clear();
+            for (Request r : getRequest) {
+                if (r.getDriverUserName() != null) {
+                    // If the request has been confirmed by the rider
+                    // Dont't display it
+                    searchRequestList.add(r);
+                }
             }
             // Notify the adapter things is changed
             searchRequestAdapter.clear();
@@ -46,7 +63,14 @@ public class DriverSearchRequestActivity extends AppCompatActivity implements Vi
         }
     });
 
-    private int searchOption;
+    // Request controller for confirming request
+    // TODO data consistent
+    private RequestController confirmedRequestController = new RequestController(new OnAsyncTaskCompleted() {
+        @Override
+        public void onTaskCompleted(Object o) {
+            Request request = (Request) o;
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,8 +107,8 @@ public class DriverSearchRequestActivity extends AppCompatActivity implements Vi
         searchRequestListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intentDriverAcceptRequest = new Intent(DriverSearchRequestActivity.this, DriverAcceptRequestActivity.class);
-                startActivity(intentDriverAcceptRequest);
+                // Open dialog
+                openRequestInfoDialog(searchRequestList.get(position));
             }
         });
     }
@@ -92,18 +116,60 @@ public class DriverSearchRequestActivity extends AppCompatActivity implements Vi
     @Override
     public void onStart() {
         super.onStart();
+        driver = FileIOUtil.loadUserFromFile(getApplicationContext());
         searchRequestAdapter = new ArrayAdapter<>(this, R.layout.driver_search_list_item, searchRequestList);
         searchRequestListView.setAdapter(searchRequestAdapter);
     }
 
     @Override
     public void onClick(View view) {
-        if (view == searchButton ) {
+        if (view == searchButton) {
             if (searchOption == 0) {
                 //TODO geo-filter
             } else if (searchOption == 1) {
+                // If search by keyword
                 requestController.searchRequestByKeyword(searchContextEditText.getText().toString());
             }
         }
     }
+
+    private void openRequestInfoDialog(final Request request) {
+        // TODO get estimated fare price and description of the request
+        String estimatedFare = request.getEstimatedFare().toString();   // replace 100 with estimated price
+        String description = request.getRequestDescription();   // replace hello with actual request description
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(DriverSearchRequestActivity.this);
+        builder.setTitle("Request Information")
+                .setMessage("Estimated Fare: " + estimatedFare + "\\n" + "Description" + description)
+                .setNeutralButton(R.string.dialog_view_map_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Serialize the request to pass it over the intent
+                        Intent intentDriverMain = new Intent(DriverSearchRequestActivity.this, BrowseRequestRouteActivity.class);
+                        // http://stackoverflow.com/questions/2736389/how-to-pass-an-object-from-one-activity-to-another-on-android
+                        intentDriverMain.putExtra("request", RequestIntentUtil.serializer(request));   // TODO replace testRequest with actuall request object
+                        startActivity(intentDriverMain);
+                    }
+                })
+                .setNegativeButton(R.string.dialog_cancel_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                })
+                .setPositiveButton(R.string.dialog_ok_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    // Driver confirms request
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Confirm request task
+                        confirmedRequestController.driverConfirmRequest(request, driver.getUserName());
+                        // Then go to browse request activity
+                        Intent intent = new Intent(DriverSearchRequestActivity.this, DriverBrowseRequestActivity.class);
+                        startActivity(intent);
+                    }
+        });
+        // Create & Show the AlertDialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
