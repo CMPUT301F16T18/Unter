3a93daf11ae6ca363eadfd21abbf0f55c0aac1a9
Michael Lin <linzichunzf@hotmail.com>
2016-11-08 15:05:01 -0700

    add map view

--- app/src/main/java/ca/ualberta/cs/unter/view/DriverMainActivity.java
+++ app/src/main/java/ca/ualberta/cs/unter/view/DriverMainActivity.java
@@ -11,11 +11,17 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import ca.ualberta.cs.unter.R;
+import ca.ualberta.cs.unter.model.User;
+import ca.ualberta.cs.unter.util.FileIOUtil;
 
 public class DriverMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private User driver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,18 @@ public class DriverMainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         assert navigationView != null;
         navigationView.setNavigationItemSelectedListener(this);
+
+        View navHeader = navigationView.getHeaderView(0);
+
+        TextView username = (TextView) navHeader.findViewById(R.id.nav_drawer_driver_username);
+        TextView email = (TextView) navHeader.findViewById(R.id.nav_drawer_driver_email);
+
+        // Get user profile
+        driver = FileIOUtil.loadUserFromFile(getApplicationContext());
+
+        // Set drawer text
+        username.setText(driver.getUserName());
+        email.setText(driver.getEmailAddress());
     }
 
     @Override
@@ -66,10 +84,10 @@ public class DriverMainActivity extends AppCompatActivity
         } else if (id == R.id.nav_request) {
             Intent intentDriverBrowseRequest = new Intent(this, DriverBrowseRequestActivity.class);
             startActivity(intentDriverBrowseRequest);
-        } else if (id == R.id.nav_complated_request){
+        } else if (id == R.id.nav_complated_request) {
             Intent intentDriverCompletedRequest = new Intent(this, DriverCompletedRequestActivity.class);
             startActivity(intentDriverCompletedRequest);
-        } else if (id == R.id.nav_search){
+        } else if (id == R.id.nav_search) {
             Intent intentDriverSearchRequest = new Intent(this, DriverSearchRequestActivity.class);
             startActivity(intentDriverSearchRequest);
         } else if (id == R.id.nav_logout) {
