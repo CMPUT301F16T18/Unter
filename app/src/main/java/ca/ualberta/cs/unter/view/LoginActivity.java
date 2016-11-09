39d570c34c3aa18944b2db696097c2a7349f4350
Michael Lin <linzichunzf@hotmail.com>
2016-11-08 21:19:06 -0700

    for get the include the file LOL

--- app/src/main/java/ca/ualberta/cs/unter/view/LoginActivity.java
+++ app/src/main/java/ca/ualberta/cs/unter/view/LoginActivity.java
@@ -4,14 +4,12 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Patterns;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import ca.ualberta.cs.unter.R;
@@ -56,7 +54,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         signupButton.setOnClickListener(this);
 
         // http://stackoverflow.com/questions/8323778/how-to-set-on-click-listener-on-the-radio-button-in-android
-
     }
 
     @Override
