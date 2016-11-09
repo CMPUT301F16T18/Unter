ec480f90a73414f705df42c690d2f316aced35ee
Michael Lin <linzichunzf@hotmail.com>
2016-11-08 21:28:00 -0700

    put those stuff into a separate repo

--- testscript/query_exact_match.sh
+++
@@ -1,9 +0,0 @@
-curl -XPOST http://cmput301.softwareprocess.es:8080/unter/user/_search -d '{
-    "query": {
-        "term": {
-           "userName": {
-              "value": "John"
-           }
-        }
-    }
-}'
