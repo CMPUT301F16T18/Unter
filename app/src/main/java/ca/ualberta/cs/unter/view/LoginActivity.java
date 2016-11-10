0393cdd5506d81645c74e23820b69d1e011fe7a8
Michael Lin <linzichunzf@hotmail.com>
2016-11-09 21:50:46 -0700

    replace the ugly dialog by the MD dialog

--- app/src/main/java/ca/ualberta/cs/unter/view/LoginActivity.java
+++ app/src/main/java/ca/ualberta/cs/unter/view/LoginActivity.java
@@ -1,6 +1,6 @@
 package ca.ualberta.cs.unter.view;
 
-import android.app.AlertDialog;
+import android.support.v7.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
